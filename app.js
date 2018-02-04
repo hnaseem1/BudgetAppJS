73e8bb7e1dba551f5bb38cf84de023b7faaf6ca8
Hasan Naseem <33922244+hnaseem1@users.noreply.github.com>
2018-02-04 17:16:55 -0500

    Update app.js

--- app.js
+++ app.js
@@ -106,6 +106,22 @@ var UIController = (function() {
           document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
       },
 
+      clearFields: function() {
+
+          var fields, fieldsArr;
+
+          fields = document.querySelectorAll(DOMstrings.inputDesc + ', ' + DOMstrings.inputValue);
+
+          fieldsArr = Array.prototype.slice.call(fields);
+
+          fieldsArr.forEach(function(current, index, array) {
+            current.value = "";
+
+          });
+
+          fieldsArr[0].focus();
+      },
+
       getDOMstrings: function() {
         return DOMstrings;
       }
@@ -145,12 +161,16 @@ var controller = (function(budgetCtrl, UICtrl) {
         // 3. Add the new item to the UI
         UICtrl.addListItem(newItem, input.type);
 
-        // 4. Calculate the budget
+        // 4. Clear the field
 
-        
-        
+        UICtrl.clearFields();
 
-        // 5. Display the budget
+        // 5. Calculate the budget
+
+
+
+
+        // 6. Display the budget
 
       }
 
